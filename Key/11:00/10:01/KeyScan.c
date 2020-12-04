55c427549d6ce8601f987aa8d0228b24197d1d01
常隐 <57696858+Ryzone@users.noreply.github.com>
2020-12-04 21:39:23 +0800

    Create KeyScan.c

---
+++ Key/11:00/10:01/KeyScan.c
@@ -0,0 +1,33 @@
+#include "KEYSCAN.H"
+
+unsigned char key = 0,Kick = 0;
+unsigned int Keytime;
+
+void Keyboard(unsigned char keyval)
+{
+	key = keyval;
+}
+void KeyScan()
+{
+	static unsigned char Keybuf;
+	unsigned char temp;
+
+	P3 = 0x0F;
+	temp = P3 & 0x0F ^ 0x0F;
+	P3 = 0xF0;
+	temp |= P3 & 0xF0 ^ 0xF0;
+	
+	if(Keybuf ^ temp)
+	{
+		if(Keytime >= 80)
+		{
+			if(Keytime >= 1200)Kick = 2;
+			else Kick = 1;
+			Keyboard(Keybuf);
+		}
+		
+		Keybuf = temp;
+		
+		Keytime = 0;
+	}
+}
