3e56d893d94ac8a4f955433bf0897e7591b295c5
常隐 <57696858+Ryzone@users.noreply.github.com>
2020-12-04 19:09:10 +0800

    Update KeyScan.c

--- Key/01:00/01:10/KeyScan.c
+++ Key/01:00/01:10/KeyScan.c
@@ -1,6 +1,7 @@
 #include "KEYSCAN.H"
 
-unsigned char key = 0;
+sbit BTN = P3^0;
+bit key = 0;
 
 void Delay80ms()
 {
@@ -19,23 +20,9 @@ void Delay80ms()
 		} while (--j);
 	} while (--i);
 }
-void Button(unsigned char keyval)
-{
-	switch(keyval)
-	{
-		case 0x01 : key = 1;break;
-		case 0x02 : key = 2;break;
-		case 0x04 : key = 3;break;
-		case 0x08 : key = 4;break;
-	}
-}
 void KeyScan()
 {
-	P3 = 0x0F;
-	if(P3 & 0x0F ^ 0x0F)
-	{
-		Delay80ms();
-		if(P3 & 0x0F ^ 0x0F)
-		Button(P3 & 0x0F ^ 0x0F);
-	}
+	BTN = 1;
+	Delay80ms();
+	if(!BTN)key = 1;
 }
