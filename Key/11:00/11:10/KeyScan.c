4c0456f8d7db6ec6061d1e9576745c6e51683521
常隐 <57696858+Ryzone@users.noreply.github.com>
2020-12-06 14:52:00 +0800

    Update KeyScan.c

--- Key/11:00/11:10/KeyScan.c
+++ Key/11:00/11:10/KeyScan.c
@@ -1,11 +1,16 @@
+#include "KEYSCAN.H"
+
+unsigned char key = 0,Kick = 0;
+unsigned int Keytime;
+
 void KeyScan()
 {
 	static unsigned char Keybuf;
 	unsigned char temp;
 
-	P3 = 0x0F;
+	P3 = 0x0F;P0 = 0;	//延时废话一句，但是必须有才能保证相邻组合键能有效果
 	temp = P3 & 0x0F;
-	P3 = 0xF0;
+	P3 = 0xF0;P0 = 0;	//延时废话一句，但是必须有才能保证相邻组合键能有效果
 	temp |= P3 & 0xF0;
 	
 	temp = ~temp;
@@ -23,14 +28,14 @@ void KeyScan()
 				return ;
 			}
 			Kick = 0;
-			Keyboard(Keybuf);
+			key = Keybuf;
 		}
 		Keybuf = temp;
 		Keytime = !Keytime;
 	}
 	else if(Keytime >= 500 && (Keytime % 50) == 0 && Keytime < 1000)
 	{
-		Keyboard(temp);
+		key = Keybuf;
 		Kick = 3;
 		if(Keytime >= 800)Keytime = 1000;
 	}
